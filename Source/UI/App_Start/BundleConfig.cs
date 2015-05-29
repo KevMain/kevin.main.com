a5e1f509802c82a8d3d79d30a17033bf794ce74d
Kevin Main <kevmain@outlook.com>
2015-05-29 22:08:40 +0100

    Added styles and scripts to support site template

--- Source/UI/App_Start/BundleConfig.cs
+++ Source/UI/App_Start/BundleConfig.cs
@@ -8,24 +8,17 @@ namespace kevin_main.com
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Assets/Styles").Include(
+                      "~/Assets/Styles/bootstrap.css",
+                      "~/Assets/Styles/style.css",
+                      "~/Assets/Styles/site.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/Assets/Scripts/ltIE9").Include(
+                      "~/Assets/Bootstrap/js/html5shiv.min.js",
+                      "~/Assets/Bootstrap/js/respond.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/Assets/Scripts/modernizr").Include(
+                      "~/Assets/Scripts/modernizr.custom.js"));
         }
     }
 }
