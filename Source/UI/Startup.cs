6a82cf6e58afdd4357494ffdd42025008d8c7f5b
Kevin Main <kevmain@outlook.com>
2015-05-29 21:24:17 +0100

    Created base MVC project

---
+++ Source/UI/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kevin_main.com.Startup))]
+namespace kevin_main.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
