c597307d437101ba19de740e4cf4ba23a27f9cdf
Kevin Main <kevmain@outlook.com>
2015-06-04 12:31:38 +0100

    First go at contact form

--- Source/UI/Controllers/HomeController.cs
+++ Source/UI/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using kevin_main.com.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +16,37 @@ namespace kevin_main.com.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(EmailFormModel model)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("kevmain@outlook.com")); 
+                message.From = new MailAddress("kevmain@outlook.com");
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+//                using (var smtp = new SmtpClient())
+//                {
+//                    var credential = new NetworkCredential
+//                    {
+//                        UserName = "user@outlook.com",  // replace with valid value
+//                        Password = "password"  // replace with valid value
+//                    };
+//                    smtp.Credentials = credential;
+//                    smtp.Host = "smtp-mail.outlook.com";
+//                    smtp.Port = 587;
+//                    smtp.EnableSsl = true;
+//                    await smtp.SendMailAsync(message);
+//                    return RedirectToAction("Sent");
+//                }
+            }
+            
+            return View(model);
         }
     }
 }
